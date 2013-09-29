1f0cf4c5fda84ffaf35d86022256919f3c91c9fe
Matthew Kastor <matthewkastor@gmail.com>
2013-09-28 23:06:52 -0400

    rebuild

--- docs/vsdoc/OpenLayersAll.js
+++ docs/vsdoc/OpenLayersAll.js
@@ -26,6 +26,15 @@
 
     window.atropa = {
         /// <summary></summary>
+        /// <field name="inquire" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-inquire/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="arrays" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-arrays/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="customErrors" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-customErrors/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
         /// <returns type="atropa"/>
                 
     };
